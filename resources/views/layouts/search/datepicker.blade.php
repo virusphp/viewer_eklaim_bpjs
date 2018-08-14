0e7a4d80f4da7841ff141bb250c3b5abecf50bd5
Andrianext <nexter.three@gmail.com>
2018-08-14 02:37:54 +0000

    update search datepicker

--- resources/views/layouts/search/datepicker.blade.php
+++ resources/views/layouts/search/datepicker.blade.php
@@ -1,26 +1,27 @@
 <div class="form-inline float-right">
     <form id="search" action="{{ $route }}" class="form-inline" role="search" >
-        <div class="controls">
+        <div class="form-group">
             <div class='input-group date {{ $errors->has('tgl') ? 'has-error' : '' }}' id='datetimepicker'>
-                <input type='text' name="tgl" class="form-control" placeholder="Tanggal..." />
-                <span class="input-group-addon">
-                    <span class="fa fa-calendar">
+                <input type='text' name="tgl" class="form-control" placeholder="Tanggal..." />              
+                <div class="input-group-append">
+                    <span class="input-group-text input-group-addon">
+                        <i class="fa fa-calendar"></i>
                     </span>
-                </span>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-search"></i>
+                    </button>
+		        </div>
             </div>
-        </div>
-        <button type="submit" class="btn btn-secondary">
-            <i class="fa fa-search"></i>
-        </button>
+        </div>          
     </form> 
     <form id="search"action="{{ $route }}" class="form-inline" role="search" >
-        <div class="controls">
+        <div class="form-group col-md-12">
             <div class='input-group date {{ $errors->has('search') ? 'has-error' : '' }}'>
                 <input type='text' name="search" class="form-control" placeholder="Cari..."/>
+                <div class="input-group-append">                   
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>
+		        </div>
             </div>
-        </div>
-        <button type="submit" class="btn btn-secondary">
-            <i class="fa fa-search"></i>
-        </button>
+        </div>       
     </form>
 </div>

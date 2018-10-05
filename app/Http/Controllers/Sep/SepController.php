f914f9e81a99d4b118a905781b422ea60928d9b9
virusphp <packercyber@gmail.com>
2018-10-05 13:37:15 +0700

    update empty sep

--- app/Http/Controllers/Sep/SepController.php
+++ app/Http/Controllers/Sep/SepController.php
@@ -32,8 +32,8 @@ class SepController extends Controller
             $data = $reg->getSearch($request);
             foreach($data as $q) {
                 $tgl = new DateTime($q->tgl_reg);
-                dd(empty($q->no_sjp), $q->no_sjp);
-                if (empty($q->no_sjp)) {
+                // dd(empty($q->no_sjp), $q->no_sjp == "");
+                if (empty($q->no_sjp) == "" || $q->no_sjp <= 15) {
                     $button = '<button type="button" value="'.$q->no_reg.'" class="btn btn-sm btn-success" id="edit-item" data-item="'.$q->no_reg.'">Buat</button>
                                <button type="button" class="btn btn-sm btn-warning" id="edit-sep" disabled>Edit</button>';
                 } else {

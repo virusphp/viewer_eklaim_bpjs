7a94b1cee73208b5df37cb310639abc162a2f86b
virusphp <packercyber@gmail.com>
2018-11-06 07:29:12 +0700

    update penamaan fktp jadi faskes

--- resources/views/simrs/sep/index.blade.php
+++ resources/views/simrs/sep/index.blade.php
@@ -12,14 +12,14 @@
     <li class="breadcrumb-item active">Index</li>
     <li class="breadcrumb-menu d-md-down-none">
         <div class="btn-group" role="group" aria-label="Button group">
-            <div class="col-md-5 col-form-label form-inline">
+            <div class="col-md-6 col-form-label form-inline">
                 <div class="form-check form-check-inline mr-1">
                     <input class="form-check-input" type="radio" id="fktp" value="1" name="fktp" checked>
-                    <label class="form-check-label" for="inline-radio1">Fktp 1</label>
+                    <label class="form-check-label" for="inline-radio1">Faskes 1</label>
                 </div>
                 <div class="form-check form-check-inline mr-1">
                     <input class="form-check-input" type="radio" id="fktp" value="2" name="fktp">
-                    <label class="form-check-label" for="inline-radio2">Fktp 2</label>
+                    <label class="form-check-label" for="inline-radio2">Faskes 2</label>
                 </div>
             </div>
             <input name="cek-no-kartu" id="cek-no-kartu" value="" class="form-control" placeholder="Scan No Kartu..." type="text">

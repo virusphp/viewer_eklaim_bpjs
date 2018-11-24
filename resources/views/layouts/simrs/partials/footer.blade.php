d01f202f5073486a82fdc92f16cfdc8356b692dc
virusphp <packercyber@gmail.com>
2018-11-24 10:38:44 +0700

    update version lagi

--- resources/views/layouts/simrs/partials/footer.blade.php
+++ resources/views/layouts/simrs/partials/footer.blade.php
@@ -5,6 +5,6 @@
     </div>
     <div class="ml-auto">
       <span>Modified by</span>
-      <a href="http://epasien.rsudkraton.com">IT RSUD Kraton</a>
+      <a href="http://epasien.rsudkraton.com">IT RSUD Kraton | Version 1.0</a>
     </div>
   </footer>

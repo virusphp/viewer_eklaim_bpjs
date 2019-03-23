2f9fa0027c3434412e61926866a2820e6734d60c
virusphp <packercyber@gmail.com>
2019-03-23 08:20:32 +0700

    Update every day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('update:bed')->everyMinute();
+        $schedule->command('update:bed')->daily();
     }
 
     /**

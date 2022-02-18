283abc9eeaa770e4a095f1eef2bcec8f73eeaf46
virusphp <packercyber@gmail.com>
2022-02-18 09:22:05 +0700

    update viewer

--- user_login_sep.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO [user_login_sep] VALUES (1, 'virusphp', 'Slamet Sugandi', '$2y$10$riP986gESv8IYK354RDuWeBwY65lhoTaATGTSwCeNm8pHFVboRNP6', 'developer', NULL, '2018-10-15 01:18:50', '2018-10-15 01:18:50', NULL);
-GO
